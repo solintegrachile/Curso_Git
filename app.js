8b1b466d617b56f1500c86d10e608efb2f546453
Marco Antonio Sanchez Bustos <38870221+solintegrachile@users.noreply.github.com>
2021-11-15 16:04:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 /*Motor de Plantillas cn el Framework EJS para Express */
 app.set('view engine','ejs');
